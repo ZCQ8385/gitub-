76de9b9627cbf7d4286355e5efb315d44b937a9b
zhangchengqian <1828317848@qq.com>
2024-11-15 16:03:11 +0800

    轮播图

--- src/api/home.js
+++ src/api/home.js
@@ -1,6 +1,22 @@
 import httpInstance from "@/utils/http";
 
-export const getBannerAPI = () => httpInstance.get("/home/banner");
+// export function getBannerAPI(params = {}) {
+//   // 默认为1 商品为2
+//   const { distributionSite = "1" } = params;
+//   return httpInstance({
+//     url: "/home/banner",
+//     params: {
+//       distributionSite,
+//     },
+//   });
+// }
+
+export const getBannerAPI = (params = {}) => {
+  const { distributionSite = "1" } = params;
+  return httpInstance.get("/home/banner", {
+    params: { distributionSite },
+  });
+};
 
 export const findNewAPI = () => httpInstance.get("/home/new");
 
